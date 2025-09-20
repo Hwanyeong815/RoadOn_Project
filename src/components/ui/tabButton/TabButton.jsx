0551640309c6715882277f920fbc45a07659f00b
Hwanyeong815 <viggitore@gmail.com>
2025-09-20 17:47:56 +0900

    0920 KHY 03
    mypage/css

--- src/components/ui/tabButton/TabButton.jsx
+++ src/components/ui/tabButton/TabButton.jsx
@@ -2,7 +2,7 @@ import './style.scss';
 
 const TabButton = ({ label, onClick, isActive }) => {
     return (
-        <button className={`button tab-button ${isActive ? 'g' : ''}`} onClick={onClick}>
+        <button className={`tab-button ${isActive ? 'g' : ''}`} onClick={onClick}>
             {label || '빈칸'}
         </button>
     );
