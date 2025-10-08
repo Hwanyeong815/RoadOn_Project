d768a515e816fb1b310ab0e3114e2492f841ccec
lluunak <bbppa24@naver.com>
2025-10-08 14:37:32 +0900

    1008 1차

--- src/components/tour/tourDetail/detailBody/DetailTitle.jsx
+++ src/components/tour/tourDetail/detailBody/DetailTitle.jsx
@@ -5,14 +5,11 @@ import useTourStore from '../../../../store/tourStore';
 import MiniReviewItem from '../../../hotels/hotelsDetail/MiniReviewItem';
 import WishButton from '../../../ui/wishbutton/WishButton';
 
-import { useNavigate } from 'react-router-dom';
-import { openWishlistShortcut } from '../../../ui/swal/presets';
-
 const DetailTitle = ({ tourData }) => {
     const reviews = useTourStore((state) => state.reviews);
     const getTourHighRatedReviews = useTourStore((state) => state.getTourHighRatedReviews);
     if (!tourData) return null;
-    const navigate = useNavigate();
+
     const { title, subtitle, desc, id } = tourData;
     const miniReviews = getTourHighRatedReviews(id, 3);
 
@@ -29,18 +26,9 @@ const DetailTitle = ({ tourData }) => {
                     </p>
                 </div>
                 <div className="more-btn-a">
-                    <img src="/images/icon/share.svg" className="share-btn" alt="공유" />
-                    {/* <img src="/images/icon/like.svg" alt="찜하기" /> */}
-                    <div className="wish-overlay" onClick={(e) => e.stopPropagation()}>
-                        <WishButton
-                            type="tour"
-                            id={tourData.id}
-                            data={tourData}
-                            onWish={(added) => {
-                                if (added) openWishlistShortcut({ navigate });
-                            }}
-                        />
-                    </div>
+                        <img src="/images/icon/share.svg" className='share-btn' alt="공유" />
+                        {/* <img src="/images/icon/like.svg" alt="찜하기" /> */}
+                        <WishButton className='wish-hotel-btn'/>
                 </div>
             </article>
             {/* <DetailBotReviewsItem tourData={tourData} /> */}
